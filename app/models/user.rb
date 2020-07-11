4f94226225dcb456822c7fe3feb3f58025cf4f01
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-07-11 17:02:33 +0900

    ログインできるよう変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-    def self.from_omniauth(auth)
-        where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
-      end
-      
-      def self.create_from_omniauth(auth)
-        create! do |user|
-          user.provider = auth["provider"]
-          user.uid = auth["uid"]
-          user.name = auth["info"]["nickname"]
-        end
-      end
+  def self.from_omniauth(auth)
+      create_from_omniauth(auth)
+  end
+    
+  def self.create_from_omniauth(auth)
+    find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["nickname"]
+    end
+  end
 end
